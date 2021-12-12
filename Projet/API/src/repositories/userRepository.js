610aef4fd20bdf40751512f10035f58afbd262cf
Alper Ozkan <alper.ozkan.p@gmail.com>
2021-12-12 20:38:57 +0100

    Completed userRepository

--- Projet/API/src/repositories/userRepository.js
+++ Projet/API/src/repositories/userRepository.js
@@ -21,6 +21,41 @@ class UserRepository {
         user.id = uuid();
         this.db.push("/users[]", user)
     }
+
+    get(id) {
+        const users = this.getAll();
+        return _.find(users, { id });
+    }
+
+    update(id, user) {
+        if (user.id !== id)
+            throw new ValidationError('You cannot change the identifier.');
+
+        checkUser(user);
+        const path = this.getIdPath(id);
+        if (path == null) {
+            throw new ValidationError('This user does not exists');
+        }
+
+        this.db.push(path, user);
+
+        return user;
+    }
+
+    delete(id) {
+        const path = this.getIdPath(id);
+        if (path != null)
+            this.db.delete(path);
+    }
+
+    getIdPath(id) {
+        const users = this.getAll();
+        const index = _.findIndex(users, { id });
+        if (index == -1)
+            return null;
+
+        return '/users[' + index + ']';
+    }
 }
 
 module.exports = UserRepository;
